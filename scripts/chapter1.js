2a25926a453efa2797d43779e179888170b7a986
Finnvb <finnvb@gmail.com>
2021-12-16 16:07:18 +0100

    .

--- scripts/chapter1.js
+++ scripts/chapter1.js
@@ -1,1 +1,16 @@
-console.log("werkpls");
+let subchapterIntro = document.getElementById("subchapter11");
+let subchapterIntroText = document.getElementById("subchapterIntroText");
+
+
+subchapterIntro.addEventListener("click", test);
+
+function test(){
+    console.log("hehe");
+    subchapterCircle.style.display='none';
+    subsubchapterCircle.style.display='block';
+}
+
+
+let subsubchapterCircle = document.getElementById("subsubchapterCircle");
+let subchapterCircle = document.getElementById("subchapterCircle");
+
